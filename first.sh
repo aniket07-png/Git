b7328ee8c6a63c016d14330a03fb71efe344376d
aniket07-png <aniket9369020880@gmail.com>
2025-12-11 11:15:01 +0530

    first

---
+++ first.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#This is my first script
+
+echo "TWS : Today i am about to teach you how to write script using shell scripting"
+
+echo "Me : I am very excited right now"
+
+echo "TWS : So lets start with introduction"
+
+<< comment
+This 
+command
+use
+for
+multi
+line
+comments
+comment
+
+a="Aniket"
+
+echo "The name of this file owner is $a and this script is created on the $(date)"
+
+echo "Enter your name: "
+
+read username
+
+echo "You entered $username"
+
+#for i in range(1,11):
+	
+echo "The characters in $0 and $1"
+
