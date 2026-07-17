7154b199a0d8375a5f31ff3644f35ce911a17e1c
Aniket Kumar <aniket9369020880@gmail.com>
2026-07-17 11:23:41 +0530

    Implement package check and service start
    Added package installation and service management logic.

--- if_elif_else.sh
+++ if_elif_else.sh
@@ -16,3 +16,24 @@ else
 	echo "You got less than 50%"
 
 fi #to close the if statement
+
+
+read -p "Enter the package name:- " Package
+
+echo "Checking if the package is already installed"
+
+if dpkg -s $Package >/dev/null 2>&1
+then
+        echo "$Package already installed"
+        exit 1
+else
+        echo "Updating system and Installing $Package"
+fi
+
+sudo apt-get update
+sudo apt install $Package -y
+
+read -p "Enter the service name:- " Service
+
+sudo systemctl start $Service
+systemctl status $Service
